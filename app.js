304c0f58a5a043d5b85c470e7a5601916e16d0c2
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-07-01 22:59:44 +0600

    bug  fix

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
 // Import external module
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
+
 // Initialize app
 const app = express();
+
 // PORT initiate
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
 // Enable cors
 app.use(cors());
 
 // Parse request
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // Connection URL
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u07m6.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+const url = process.env.DB_CONNECTION_URL;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Database name
+const dbName = process.env.DB_NAME;
 
 async function run() {
   try {
     await client.connect();
-    const tasksCollection = client.db("task-manager").collection("tasks");
+    const tasksCollection = client.db(dbName).collection("tasks");
+
     // Create task
-    app.post("/create", async (req, res) => {
+    app.post("/task/create", async (req, res) => {
       const data = { ...req.body, createdAt: new Date() };
       const result = await tasksCollection.insertOne(data);
       res.send(result);
     });
+
     // Get all task
-    app.get("/all", async (req, res) => {
+    app.get("/task/all", async (req, res) => {
       const tasks = await tasksCollection.find({}).toArray();
       res.send(tasks);
     });
+
     // Update task
-    app.put("/all/:id", async (req, res) => {
+    app.put("/task/:id", async (req, res) => {
       const data = req.body;
       const id = req.params.id;
       const tasks = await tasksCollection.updateOne(
@@ -46,8 +53,9 @@ async function run() {
       );
       res.send(tasks);
     });
+
     // Delete task
-    app.delete("/all/:id", async (req, res) => {
+    app.delete("/task/:id", async (req, res) => {
       const id = req.params.id;
       const result = await tasksCollection.deleteOne({ _id: ObjectId(id) });
       res.send(result);
@@ -55,6 +63,7 @@ async function run() {
   } finally {
   }
 }
+
 run().catch(console.dir);
 
 // Initial route
@@ -63,6 +72,6 @@ app.get("/", async (req, res) => {
 });
 
 // Listen to app
-app.listen(port, (err) => {
-  if (!err) console.log(`Server successfully running at  PORT ${port}`);
+app.listen(PORT, (err) => {
+  if (!err) console.log(`Server successfully running at  PORT ${PORT}`);
 });
